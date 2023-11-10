a5bb644125a108231231bc16feb8e465a36786ed
Gabe <gabeshin.ts@gmail.com>
2023-11-10 23:25:22 +0900

    feat: add eslint and formatter

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,116 +2,135 @@ local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
 lsp.on_attach(function(client, bufnr)
-    lsp.default_keymaps({ buffer = bufnr })
+	lsp.default_keymaps({ buffer = bufnr })
 end)
 
-local mason = require('mason')
-local mason_lspconfig = require('mason-lspconfig');
+local mason = require("mason")
+local mason_lspconfig = require("mason-lspconfig")
 local mason_tool_installer = require("mason-tool-installer")
 
 -- Set up Icons
 mason.setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-        },
-    }
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
 
 -- List of LSP to install
 mason_lspconfig.setup({
-    ensure_installed = {
-        'tsserver',
-        'rust_analyzer',
-        'lua_ls',
-        'html',
-        'cssls',
-        'tailwindcss',
-        'graphql',
-        'prismals',
-        'pyright',
-    },
-    automatic_installation = true,
-    handlers = {
-        lsp.default_setup,
-    }
+	ensure_installed = {
+		"tsserver",
+		"rust_analyzer",
+		"lua_ls",
+		"html",
+		"cssls",
+		"tailwindcss",
+		"graphql",
+		"prismals",
+		"pyright",
+	},
+	automatic_installation = true,
+	handlers = {
+		lsp.default_setup,
+	},
 })
 
 -- List of Formatters/Linters to install
 mason_tool_installer.setup({
-    ensure_installed = {
-        "prettier",
-        "stylua",
-        "isort",
-        "black",
-        "pylint",
-        "eslint_d",
-    },
+	ensure_installed = {
+		"prettier",
+		"stylua",
+		"isort",
+		"black",
+		"pylint",
+		"eslint_d",
+	},
 })
 
 -- LSP config setup
-require("lspconfig").lua_ls.setup {
-    settings = {
-        Lua = {
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = { 'vim' },
-            },
-        },
-    }
-}
+require("lspconfig").lua_ls.setup({
+	settings = {
+		Lua = {
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+		},
+	},
+})
 
-
-local cmp = require('cmp')
+local cmp = require("cmp")
 cmp.setup({
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    })
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+	}),
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+	suggest_lsp_servers = false,
+	sign_icons = {
+		error = "E",
+		warn = "W",
+		hint = "H",
+		info = "I",
+	},
 })
 
 lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
-    -- go to definition
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    -- show all references
-    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-    -- show hover information
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    -- search for symbols in the workspace
-    vim.keymap.set("n", "gw", function() vim.lsp.buf.workspace_symbol() end, opts)
-    -- open a floating window with diagnostic information
-    vim.keymap.set("n", "gvd", function() vim.diagnostic.open_float() end, opts)
-    -- jump to the next diagnostic (error, warning)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    -- jump to the previous diagnostic (error, warning)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    -- trigger code actions for the current context
-    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-    -- rename the symbol across the entire workspace
-    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-    -- show signature help when in insert mode
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+	local opts = { buffer = bufnr, remap = false }
+	-- go to definition
+	vim.keymap.set("n", "gd", function()
+		vim.lsp.buf.definition()
+	end, opts)
+	-- show all references
+	vim.keymap.set("n", "gr", function()
+		vim.lsp.buf.references()
+	end, opts)
+	-- show hover information
+	vim.keymap.set("n", "K", function()
+		vim.lsp.buf.hover()
+	end, opts)
+	-- search for symbols in the workspace
+	vim.keymap.set("n", "gw", function()
+		vim.lsp.buf.workspace_symbol()
+	end, opts)
+	-- open a floating window with diagnostic information
+	vim.keymap.set("n", "gvd", function()
+		vim.diagnostic.open_float()
+	end, opts)
+	-- jump to the next diagnostic (error, warning)
+	vim.keymap.set("n", "[d", function()
+		vim.diagnostic.goto_next()
+	end, opts)
+	-- jump to the previous diagnostic (error, warning)
+	vim.keymap.set("n", "]d", function()
+		vim.diagnostic.goto_prev()
+	end, opts)
+	-- trigger code actions for the current context
+	vim.keymap.set("n", "<leader>ca", function()
+		vim.lsp.buf.code_action()
+	end, opts)
+	-- rename the symbol across the entire workspace
+	vim.keymap.set("n", "<leader>rn", function()
+		vim.lsp.buf.rename()
+	end, opts)
+	-- show signature help when in insert mode
+	vim.keymap.set("i", "<C-h>", function()
+		vim.lsp.buf.signature_help()
+	end, opts)
 end)
 
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true,
+	virtual_text = true,
 })
--- lua/gabe/packer.lua
+++ lua/gabe/packer.lua
@@ -1,62 +1,71 @@
 -- This file can be loaded by calling `lua require("plugins")` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function(use)
-    -- Packer can manage itself
-    use "wbthomason/packer.nvim"
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-    use {
-        "nvim-telescope/telescope.nvim", tag = "0.1.4",
-        requires = { { "nvim-lua/plenary.nvim" } }
-    }
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.4",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-    -- Color Scheme - kanagawa
-    use "rebelot/kanagawa.nvim"
+	-- Color Scheme - kanagawa
+	use("rebelot/kanagawa.nvim")
 
-    -- Treesitter
-    use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+	-- Treesitter
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
-    -- Harpoon
-    use {
-        "ThePrimeagen/harpoon",
-        requires = { { "nvim-lua/plenary.nvim" } }
-    }
-    -- Undotree
-    use "mbbill/undotree"
+	-- Harpoon
+	use({
+		"ThePrimeagen/harpoon",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	-- Undotree
+	use("mbbill/undotree")
 
-    -- Fugitive (Git)
-    use "tpope/vim-fugitive"
+	-- Fugitive (Git)
+	use("tpope/vim-fugitive")
 
-    -- LSP zero
-    use {
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v3.x",
-        requires = {
-            -- LSP
-            { "neovim/nvim-lspconfig" },
-            -- Autocompletion
-            { "hrsh7th/nvim-cmp" },
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "L3MON4D3/LuaSnip" },
-        }
-    }
+	-- LSP zero
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		requires = {
+			-- LSP
+			{ "neovim/nvim-lspconfig" },
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "L3MON4D3/LuaSnip" },
+		},
+	})
 
-    use {
-        "williamboman/mason.nvim",
-        requires = {
-            { "williamboman/mason-lspconfig.nvim" },
-            { "WhoIsSethDaniel/mason-tool-installer.nvim" }
-        }
-    }
+	use({
+		"williamboman/mason.nvim",
+		requires = {
+			{ "williamboman/mason-lspconfig.nvim" },
+			{ "WhoIsSethDaniel/mason-tool-installer.nvim" },
+		},
+	})
 
-    -- Github Copilot
-    use "github/copilot.vim"
+	-- Github Copilot
+	use("github/copilot.vim")
 
-    -- Formatter
-    use "stevearc/conform.nvim"
+	-- Formatter
+	use("stevearc/conform.nvim")
 
-    -- Linter
-    use "mfussenegger/nvim-lint"
+	-- Linter
+	use("mfussenegger/nvim-lint")
+
+	-- Vim Tree
+	use({
+		"nvim-tree/nvim-tree.lua",
+		requires = {
+			{ "nvim-tree/nvim-web-devicons" },
+		},
+	})
 end)
