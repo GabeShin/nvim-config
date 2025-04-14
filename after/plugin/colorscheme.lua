046c044a102c0a7ee8c3b737da59a141bb59d394
Gabe <gabeshin.ts@gmail.com>
2025-04-14 15:07:20 +0900

    clean slate

--- after/plugin/colorscheme.lua
+++
@@ -1,7 +0,0 @@
-function MakeBackgroundTransparent()
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-end
-
-vim.cmd("colorscheme kanagawa")
-MakeBackgroundTransparent()
--- after/plugin/ven-selector.lua
+++
@@ -1,9 +0,0 @@
-require("venv-selector").setup({
-	settings = {
-		search = {
-			find_code_venvs = {
-				command = "fd /bin/python$ /Users/gabe/person/ --full-path",
-			},
-		},
-	},
-})
