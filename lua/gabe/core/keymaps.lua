ca7479ad0e58d7b0f8709cdf8de7343713c6ecc3
Gabe <gabeshin.ts@gmail.com>
2025-04-15 07:31:27 +0900

    delete remap

--- lua/gabe/core/keymaps.lua
+++ lua/gabe/core/keymaps.lua
@@ -17,8 +17,9 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
--- delete single character without copying into register
--- keymap.set("n", "x", '"_x')
+-- delete without copying into register
+keymap.set("n", "x", '"_x')
+keymap.set("n", "d", '"_d')
 
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
